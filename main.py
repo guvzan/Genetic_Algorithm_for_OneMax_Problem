84ef8fae15d292570e6d9538a1d78ee4e419bb06
guvzan <vernmaks@gmail.com>
2023-02-18 10:45:23 +0200

    Corrections

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import random as r
+import copy as c
 
 GENOM_LENGTH = 100
 POPULATION_LENGTH = 50
 CROSSING_CHANCE = 0.9
-MUTATION_CHANCE = 0
-GENERATIONS_NEEDED = 50
+MUTATION_CHANCE = 0.1
+GENERATIONS_NEEDED = 500
 
 r.seed()
 
@@ -46,6 +47,10 @@ class Population:
         """pass"""
         return max(self.individuals, key=lambda ind: ind.fitness).fitness
 
+    def get_worse(self):
+        """pass"""
+        return min(self.individuals, key=lambda ind: ind.fitness).fitness
+
     # def get_chances(self):
     #     """pass"""
     #     summa = 0
@@ -63,22 +68,23 @@ class Population:
             a1 = a2 = a3 = 0
             while a1 == a2 or a1 == a3 or a2 == a3:
                 a1, a2, a3 = r.randint(0, POPULATION_LENGTH-1), r.randint(0, POPULATION_LENGTH-1), r.randint(0, POPULATION_LENGTH-1)
-            best_individuals.append(max(self.individuals[a1], self.individuals[a2], self.individuals[a3], key=lambda individ: individ.fitness))
+            best = max(self.individuals[a1], self.individuals[a2], self.individuals[a3], key=lambda individ: individ.fitness)
+            best_individuals.append(c.deepcopy(best))
         self.individuals = best_individuals
 
-        print(self.get_sum(), 'b')
+        # print(self.get_sum(), 'b')
 
         for i in self.individuals:
             i.calc_fitness()
         # map(Individual.calc_fitness, self.individuals)
 
-        print(self.get_sum(), 'a')
+        # print(self.get_sum(), 'a')
 
     def cross_two_inds(self, list, ind1, ind2):
         point = r.randint(1, GENOM_LENGTH-1)
         ind1.genom[:point], ind2.genom[:point] = ind2.genom[:point], ind1.genom[:point]
-        ind1.calc_fitness()
-        ind2.calc_fitness()
+        # ind1.calc_fitness()
+        # ind2.calc_fitness()
         list.append(ind1)
         list.append(ind2)
         return list
@@ -86,14 +92,15 @@ class Population:
     def crossing(self):
         """pass"""
         new_individuals = []
-        for ind1, ind2 in zip(self.individuals[::2], self.individuals[1::2]):
-            print(self.individuals.index(ind1), self.individuals.index(ind2))
+        for i in range(0, POPULATION_LENGTH, 2):
             if r.random() <= CROSSING_CHANCE:
-                new_individuals = self.cross_two_inds(new_individuals, ind1, ind2)
+                self.cross_two_inds(new_individuals, self.individuals[i], self.individuals[i+1])
             else:
-                new_individuals.append(ind1)
-                new_individuals.append(ind2)
+                new_individuals.append(self.individuals[i])
+                new_individuals.append(self.individuals[i+1])
         self.individuals = new_individuals
+        for i in self.individuals:
+            i.calc_fitness()
 
 
     def mutate(self):
@@ -108,14 +115,15 @@ class Population:
         generation_number = 0
         print(f"Generation: {generation_number}")
         print(f"Best fitness: {self.get_best()}")
-        print(f"Mean fitness: {self.get_sum() / self.get_best()}\n")
+        print(f"Mean fitness: {self.get_sum() / POPULATION_LENGTH}\n")
         generation_number += 1
-        while generation_number <= GENERATIONS_NEEDED:
+        while generation_number <= GENERATIONS_NEEDED and self.get_best() != 100:
             self.tournament()
             self.crossing()
             self.mutate()
             print(f"Generation: {generation_number}")
             print(f"Best fitness: {self.get_best()}")
+            print(f"Worst fitness: {self.get_worse()}")
             print(f"Mean fitness: {self.get_sum() / POPULATION_LENGTH}\n")
             generation_number += 1
 
